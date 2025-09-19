dddcc251fd0be8a3c662db089a719554d7879ec0
sanavalos <santiago.avalos@outlook.com>
2025-09-19 20:43:53 -0300

    feat: add Empleado class

--- models/Empleado.js
+++ models/Empleado.js
@@ -1,4 +1,10 @@
 class Empleado {
+  constructor({id, nombre, rol, area}) {
+    this.id = id;          
+    this.nombre = nombre;  
+    this.rol = rol;        
+    this.area = area;      
+  }
 }
 
 module.exports = Empleado;
