776e616a5f1a9930a2255b8aed4b1353d44d54e0
sanavalos <santiago.avalos@outlook.com>
2025-09-19 20:44:52 -0300

    feat: add api empleados route

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const empleadoRoutes = require('./routes/empleadoRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +10,8 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
+
+app.use('/api/empleados', empleadoRoutes);
 
 app.listen(PORT, () => {
     console.log('SERVER RUNNING @ PORT 3000')
