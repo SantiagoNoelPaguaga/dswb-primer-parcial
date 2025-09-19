7bbf4ff2b33031e22c509775d4ce3a09bd96bc1c
sanavalos <santiago.avalos@outlook.com>
2025-09-19 20:42:21 -0300

    feat: add crear empleado controller

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -4,6 +4,16 @@ class EmpleadoController {
   constructor() {
     this.empleadoModel = empleadoModel;
   }
+
+  async crear(req, res) {
+    try {
+      const nuevoEmpleado = await this.empleadoModel.crear(req.body);
+      res.status(201).json(nuevoEmpleado);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
 }
 
 module.exports = EmpleadoController;
