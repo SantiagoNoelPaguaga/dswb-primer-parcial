d48398ee935bcd9a018e0dfc2870c89aaec0aac8
sanavalos <santiago.avalos@outlook.com>
2025-09-19 20:44:14 -0300

    feat: add empleado model crear method

--- models/EmpleadoModel.js
+++ models/EmpleadoModel.js
@@ -6,6 +6,23 @@ class EmpleadoModel {
   constructor() {
     this.rutaJSON = path.join(__dirname, '../data/empleados.json');
   }
+
+  async listar() {
+    const data = await fs.readFile(this.rutaJSON, 'utf-8');
+    return JSON.parse(data);
+  }
+
+  async crear(datos) {
+    const empleados = await this.listar();
+    const nuevoEmpleado = new Empleado({
+      id: empleados.length + 1,
+      ...datos
+    });
+    empleados.push(nuevoEmpleado);
+    await fs.writeFile(this.rutaJSON, JSON.stringify(empleados, null, 2));
+    return nuevoEmpleado;
+  }
+
 }
 
 module.exports = new EmpleadoModel();
